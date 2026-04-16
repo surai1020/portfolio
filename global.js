7f015cb5a430cc41c1fd66c15033c84ba2716e57
surai1020 <sumedharai1020@gmail.com>
2026-04-15 20:01:01 -0700

    added color option and js

---
+++ global.js
@@ -0,0 +1,83 @@
+console.log('IT’S ALIVE!');
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
+);
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// navLinks = $$('nav a');
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink.classList.add('current');
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink.classList.add('current');
+// }
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume/", title: "Resume" },
+  { url: "https://github.com/surai1020", title: "Github" }
+];
+
+let nav = document.createElement('nav');
+document.querySelector('.color-scheme').after(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    if (!url.startsWith("http")) {
+        url = BASE_PATH + url;
+    }
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+    nav.append(a);
+}
+
+const select = document.querySelector('.color-scheme select');
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+
+  document.documentElement.style.setProperty(
+    'color-scheme',
+    event.target.value
+  );
+
+  localStorage.colorScheme = event.target.value;
+});
+
+if ("colorScheme" in localStorage) {
+  document.documentElement.style.setProperty(
+    'color-scheme',
+    localStorage.colorScheme
+  );
+
+  select.value = localStorage.colorScheme;
+}
